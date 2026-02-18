3b802fbdf2820f5485cc9940053d3dbd8aa4b645
aliya <aliya.akhmedova.89@gamil.com>
2026-02-19 01:59:32 +0300

    pikmi pro max

--- Group4337/MainWindow.xaml.cs
+++ Group4337/MainWindow.xaml.cs
@@ -6,5 +6,11 @@ namespace Group4337
     {
         public MainWindow()
             => InitializeComponent();
+
+        private void Akhmedova_4337_Click(object sender, RoutedEventArgs e)
+        {
+            var Akhmedova_4337 = new _4337_Akhmedova();
+            Akhmedova_4337.ShowDialog();
+        }
     }
 }
